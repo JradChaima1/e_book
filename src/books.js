3ace4a3b74ff36c8969cb160a6cbb65115388fc5
JradChaima1 <chaimajrad1888@gmail.com>
2023-08-25 16:33:40 +0200

    show books

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+    {
+      author: 'Stephen King',
+      title: 'Holly',
+      img: img1,
+      id: 1,
+    },
+    {
+      author: 'Hanya Yanagihara ',
+      title: 'A Little Life',
+      img: img2,
+      id: 2,
+    },
+    {
+      author: 'The Four Agreements',
+      title: 'Don Miguel Ruiz',
+      img: img3,
+      id: 3,
+    },
+  ];
